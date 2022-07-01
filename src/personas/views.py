a1f7a516b6b1c1b099e0eb9060d597424fc13c53
Diego Pacori <dpacoria@unsa.edu.pe>
2022-06-30 23:23:07 -0500

    Probando la seguridad de django(diapo 4)

--- src/personas/views.py
+++ src/personas/views.py
@@ -22,8 +22,9 @@ def searchForHelp(request):
     return render(request, 'personas/search.html',{})
 
 def personasAnotherCreateView(request):
-    form = RawPersonaForm()
+    form = RawPersonaForm(request.POST)
     context = {
         'form':form,
     }
+    print(form)
     return render(request, 'personas/personasCreate2.html', context)
